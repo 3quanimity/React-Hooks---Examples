f8a86f4ba1d48a6940dcc346e008f4ccf7f8b8fd
Haythem Ben Drissia <49625533+3quanimity@users.noreply.github.com>
2020-04-14 12:41:10 +0200

    Update State.js

--- src/State.js
+++ src/State.js
@@ -1,3 +1,7 @@
+// useState allows us to make our components stateful. Whereas this previously required using a class component, 
+// hooks give us the ability to write it using just functions. It allows us to have more flexible components. 
+// In our example component, everytime you click on the h1 (bad a11y, by the way) it'll change colors. 
+// It's doing this by keeping that bit of state in a hook which is being fed in anew every render so it always has the latest state.
 import React, { useState } from "react";
 
 const StateComponent = () => {
